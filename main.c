c6097c0db971aad6a9f49ce2a00ecdb442400d6e
makstuk1410 <59333694+makstuk1410@users.noreply.github.com>
2024-01-20 21:18:35 +0100

    Dodałem komentarzy do main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-	int error = blad(argc, argv);
+	int error = blad(argc, argv); //sprawdza argument wywołania 
 	
 	if(error == 1){
 		return 1;
@@ -18,21 +18,21 @@ int main(int argc, char **argv)
 	
 	int n = argc > 1 ? atoi(argv[1]) : 11;
 	
-	lab l = createLab( n );
+	lab l = createLab( n ); //tworzy potrzebną macierz
 	if (l == NULL)
 		return 1;
 	
-	fill_lab(l);
-	rysowanie( l );
-	zamianaLabZero( l );
-	zamianaLab( l );
-	rysowanieScian( l );
-	zamiana( l );
-	printLab( l );
-	wagiLab( l );
-	najmniejszaSciezka( l );
-	zamianaLabZero( l );
-	freeLab( l );
+	fill_lab(l); //zapełnia labirynt początkowymi wartościami
+	rysowanie( l ); //pierwsza część algorytmu
+	zamianaLabZero( l ); //zamiana dla przejścia w labiryncie
+	zamianaLab( l ); //zamiana wszystkich trójek na 0
+	rysowanieScian( l ); //końcowy etap budowania labiryntu
+	zamiana( l ); //zamiana wszystkich liczb innych od 0 na 1
+	printLab( l ); //drykowanie labiryntu
+	wagiLab( l ); //nadanie wagi każdemu wierzchołku
+	najmniejszaSciezka( l ); //wypisywanie ścieżek i szukanie ich do tego
+	zamianaLabZero( l ); 
+	freeLab( l ); //zwolnienie pamięci 
 
-	return 0;
+	return 0; //zwraca zero kiedy wszystko jest w porządku
 }
